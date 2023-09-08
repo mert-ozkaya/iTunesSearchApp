95651289194e06c65701bf693fa1e974499807c6
mert-ozkaya <ozkaya-mert@hotmail.com>
2023-09-08 22:47:05 +0300

    Project created

---
+++ iTunesSearchApp/Layers/Presentation/Search/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  iTunesSearchApp
+//
+//  Created by Mert Ozkaya on 8.09.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        self.navigationController?.navigationBar.backgroundColor = .white
+    }
+
+}
